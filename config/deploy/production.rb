d9c90efdb06a88a93aacd4666eb65d16db7476b3
ken-wong <wangkun025@gmail.com>
2016-06-17 16:39:33 +0800

    Change server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,9 +7,9 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
-role :app, %w{deploy@139.196.233.121}
-role :web, %w{deploy@139.196.233.121}
-role :db,  %w{deploy@139.196.233.121}
+role :app, %w{deploy@139.196.175.233}
+role :web, %w{deploy@139.196.175.233}
+role :db,  %w{deploy@139.196.175.233}
 
 # role-based syntax
 # ==================
